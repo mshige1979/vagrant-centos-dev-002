97d7df4b432aa123173b322b416ae441ab38cd0b
shigeharu matsumoto <m_shige1979@r2.dion.ne.jp>
2014-05-11 15:57:44 +0900

    Update script.sh

--- data2/script.sh
+++ data2/script.sh
@@ -81,6 +81,6 @@ sudo chown -R zabbix.zabbix /var/log/zabbix/
 # zabbix conf copy
 sudo cp -f /vagrant/zabbix_agentd.conf /usr/share/zabbix/etc/zabbix_agentd.conf
 sudo cp -a /tmp/zabbix-2.2.2/misc/init.d/fedora/core5/zabbix_agentd /etc/init.d/.
-
+sudo cp -f /vagrant/zabbix_agentd /etc/init.d/zabbix_agentd
 
 
