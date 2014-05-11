37b1441406cba4533263ce0dc64aaff4ab9edb7f
m_shige1979 <m_shige1979@r2.dion.ne.jp>
2014-05-11 15:36:06 +0900

    add

--- data1/zabbix.conf.php
+++ data1/zabbix.conf.php
@@ -11,9 +11,9 @@ $DB["PASSWORD"]			= 'password';
 // SCHEMA is relevant only for IBM_DB2 database
 $DB["SCHEMA"]			= '';
 
-$ZBX_SERVER				= 'localhost';
+$ZBX_SERVER				= '192.168.33.111';
 $ZBX_SERVER_PORT		= '10051';
-$ZBX_SERVER_NAME		= '';
+$ZBX_SERVER_NAME		= '192.168.33.111';
 
 $IMAGE_FORMAT_DEFAULT	= IMAGE_FORMAT_PNG;
 ?>
